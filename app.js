11152a1ee28f0df6b92d44f5bfc24641271ef4b7
Stephen <stephennguyen24@gmail.com>
2024-03-15 22:26:29 +0700

    feat(app): add app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+  console.log(req);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
