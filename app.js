1737b4f7cee5d7bcdb7efd44a911f5c5d1ddceda
Stephen <stephennguyen24@gmail.com>
2024-03-15 23:12:38 +0700

    feat(routing): setup basic routing

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello');
-  console.log(req);
+  res.status(200).json({ message: 'hello', app: 'natours' });
+});
+
+app.post('/', (req, res) => {
+  res.json({ requestBody: req.body });
 });
 
 const port = 3000;
