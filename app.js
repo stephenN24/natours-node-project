dae815555f8e9979cc6ffe2ac104c63ea8c5158b
Stephen <stephennguyen24@gmail.com>
2024-03-16 22:55:37 +0700

    feat: add logic to handle GET request

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello', app: 'natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.json({ requestBody: req.body });
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
