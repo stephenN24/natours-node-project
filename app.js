83e65897f9273c4427835417d100fb7e1903cb65
Stephen <stephennguyen24@gmail.com>
2024-03-17 15:46:07 +0700

    feat(route): add logic for get/post specific tour data

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 
 const app = express();
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -15,6 +16,43 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = +req.params.id;
+  const tour = tours.find((tour) => tour.id == id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours.at(-1).id + 1;
+  const newTour = Object.assign({ id: newId }, req.body); // Create new tour
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(200).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
