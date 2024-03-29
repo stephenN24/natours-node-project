2d7579141e0d1644e8e052555a37b87995aa0ac6
Stephen <stephennguyen24@gmail.com>
2024-03-29 20:31:42 +0700

    feat(route): mount routes

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const exp = require('constants');
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
@@ -140,21 +141,19 @@ const deleteUser = (req, res) => {
 };
 
 // Routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.use('/api/v1/tours/', tourRouter);
+app.use('/api/v1/users/', userRouter);
 
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+const tourRouter = express.Router();
+const userRouter = express.Router();
 
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
+tourRouter.route('/').get(getAllTours).post(createTour);
 
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+
+userRouter.route('/').get(getAllUsers).post(createUser);
+
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 //Start server
 app.listen(port, () => {
