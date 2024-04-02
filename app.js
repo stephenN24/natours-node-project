31b43d7b6f849678078107b07f4a15ef1b8f389a
Stephen <stephennguyen24@gmail.com>
2024-04-02 21:24:33 +0700

    feat(static file): serve static file public folder

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const userRouter = require('./routes/userRoutes');
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
